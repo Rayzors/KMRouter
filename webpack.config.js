3ae05e987980a1754fc31199a90bf0dca4311eec
Kevin <kevin.manssat@hotmail.fr>
2019-03-17 14:01:24 +0100

    add source map

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
